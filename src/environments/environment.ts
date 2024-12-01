af99781796d966aaa45452d8c20d48470542fe46
Orkun Doğan <od.orkun.dogan@gmail.com>
2024-12-02 00:54:28 +0300

    environment file changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   localAPIUrl: 'http://localhost:8080',
   springApiUrl: 'https://kutez-case-study-be-spring-boot-app.onrender.com',
-  GOLD_PRICE_API_KEY: process.env['GOLD_PRICE_API_KEY'],
+  GOLD_PRICE_API_KEY: '3f62d0695f66efacc8ff2860e73f8109',
 };
