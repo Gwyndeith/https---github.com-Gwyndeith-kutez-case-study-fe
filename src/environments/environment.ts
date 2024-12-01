517eb0a45d2feb9e79ac75d19f6c7a586e5a2de2
Orkun Doğan <od.orkun.dogan@gmail.com>
2024-12-02 00:40:35 +0300

    env var update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   localAPIUrl: 'http://localhost:8080',
   springApiUrl: 'https://kutez-case-study-be-spring-boot-app.onrender.com',
+  GOLD_PRICE_API_KEY: process.env['GOLD_PRICE_API_KEY'],
 };
